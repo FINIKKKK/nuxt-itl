3994349790858068743905c9a8015d7c9ca88b99
FINIKKKK <digitallkrash@gmail.com>
2023-04-29 22:41:23 +0600

    init, test submit

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template lang="">
+  <div>
+    <form @submit.prevent="onSubmit">
+      <input type="text" placeholder="firstName" v-model="firstName" />
+      <input type="text" placeholder="lastName" v-model="lastName" />
+      <input type="text" placeholder="email" v-model="email" />
+      <input type="text" placeholder="password" v-model="password" />
+      <button type="submit">Send</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async onSubmit() {
+      const dto = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        password: this.password,
+      };
+      const { data } = await axios.post(
+        "http://127.0.0.1:8000/api/auth/register",
+        dto
+      );
+      console.log(data);
+    },
+  },
+};
+</script>
+
+<style lang=""></style>
